c0b1c436fbd9b0bf68482ba4b95dccd8eecca4a5
medinai <64745369+mizgutdina@users.noreply.github.com>
2020-12-13 17:22:35 -0600

    Adds part with drop down and filtering

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,33 +8,76 @@ d3.json("samples.json").then(function(data) {
 const dataPromise = d3.json("samples.json");
 console.log("Data Promise: ", dataPromise)
 //Up to now works
-
-//2. Create a horizontal bar chart with a dropdown menu 
-//to display the top 10 OTUs found in that individual.
-
-//Unpack JSON
-//*****HOW TO CALL THE DATA NEEDED FOR CHARTS? */
 function unpack(rows, index) {
     return rows.map(function(row) {
         return row[index];
     });
 }
+//2.Dropdown
+//(#selDataset)
+//Create init 
+function init() {
+    var selector = d3.select("#selDataset");
+    //Grab values from samples.json 
+    d3.json("samples.json").then((data) => {
+        var sampleIDNames = data.names;
+        console.log("IDs ", sampleIDNames);
+        sampleIDNames.forEach((sample) => {
+            selector.append("option").text(sample).property("value");
+        })
+        //build chart with first id name
+        buildPlot(sampleIDNames[0]);
+    })
 
-function buildPlot() {
-    d3.json("samples.json").then(function(data) {
-        //Grab values from samples.json to build the plots
+}
+init();
+
+//Event listener function
+function optionChanged(newSample) {
+//recall function inside option change (buildPlot(neewsample))
+    buildPlot(newSample);
+};
+
+//3. Create a horizontal bar chart with a dropdown menu 
+//to display the top 10 OTUs found in that individual.
+
+//Unpack JSON
+
+//Building plot by feeding the sample id value from above init function
+function buildPlot(sample) {
+    d3.json("samples.json").then((data) => {
+        //Filer function matching the id
+        var samples = data.samples 
+        var filteredData = samples.filter(sampleID => sampleID.id === sample)[0]
+        
         //Use sample_values as the values for the bar chart.
-        var sample_values = data.samples.sample_values;
-
+        var sample_values = filteredData.sample_values;
+        
         //Use otu_ids as the labels for the bar chart.
-        var otu_ids = data.samples.otu_ids;
+        var otu_ids = filteredData.otu_ids;
 
         //Use otu_labels as the hovertext for the chart.
-        var otu_labels = data.samples.otu_labels;
+        var otu_labels = filteredData.otu_labels;
 
+        console.log(sample_values);
+        console.log(otu_ids);
+        console.log(otu_labels);
+        //trace, layout go here *****USE SLICE & REVERSE to display top 10 OTUs
+        var trace1 = {
+            x: sample_values,
+            y: otu_ids,
+            text: otu_labels,
+            type: "bar"
+        };
 
-        console.log(sample_values, otu_ids, otu_labels);
-    })
+        var layout = {
+            xaxis: {"OTU ": sample_values},
+            yaxis: {otu_ids}
+        }
+        //Plotly.NewPlot("bar",trace1, layout);
+    }) 
 }
 
-buildPlot();
+//buildPlot();
+//id directly
+Plotly.NewPlot("bar",trace1, layout);
